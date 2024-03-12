800e373fcf817cb765c1e38daf07de358f098c88
oruccakir <oruccakir2525@gmail.com>
2024-03-12 08:07:38 +0300

    test file updated via unit tests

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,5 +1,8 @@
 package com.mycompany.app;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -31,8 +34,30 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
+    /* 
     public void testApp()
     {
         assertTrue( true );
     }
+*/
+    public void testFound() {
+      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+      assertTrue(new App().search(array, 4));
+    }
+
+    public void testNotFound() {
+      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+      assertFalse(new App().search(array, 5));
+    }
+
+    public void testEmptyArray() {
+      ArrayList<Integer> array = new ArrayList<>();
+      assertFalse(new App().search(array, 1));
+    }
+
+    public void testNull() {
+      assertFalse(new App().search(null, 1));
+    }
+
+
 }
