b745c55a1b13cf8c9f3f659e155d7e1cc605c00b
oruccakir <oruccakir2525@gmail.com>
2024-03-12 10:12:53 +0300

    test methods completed

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,5 +1,6 @@
 package com.mycompany.app;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -42,21 +43,42 @@ public class AppTest
 */
     public void testFound() {
       ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertTrue(new App().search(array, 4));
+      ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(2, 3));
+      ArrayList<String> strings = new ArrayList<>(Arrays.asList("hello", "hello"));
+      String str = "hell";
+      assertTrue(new App().search(array, array1, strings, str));
     }
 
     public void testNotFound() {
       ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertFalse(new App().search(array, 5));
+      ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(6,4,8,9,6,6,625));
+      ArrayList<String> strings = new ArrayList<>(Arrays.asList("car", "empty"));
+      String str = "481";
+      assertFalse(new App().search(array, array1, strings, str));
     }
 
     public void testEmptyArray() {
       ArrayList<Integer> array = new ArrayList<>();
-      assertFalse(new App().search(array, 1));
+      ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(8,58,21,96));
+      ArrayList<String> strings = new ArrayList<>(Arrays.asList("house", "com"));
+      String str = "781";
+      assertFalse(new App().search(array, array1, strings, str ));
     }
 
     public void testNull() {
-      assertFalse(new App().search(null, 1));
+      ArrayList<Integer> array = null;
+      ArrayList<Integer> array1 = null;
+      ArrayList<String> strings = null;
+      String str = null;
+      assertFalse(new App().search(array, array1, strings, str ));
+    }
+
+    public void testSubstrings() {
+      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+      ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(2, 3));
+      ArrayList<String> strings = new ArrayList<>(Arrays.asList("araba", "arasokak","aracilik","karabas"));
+      String str = "ara";
+      assertTrue(new App().search(array, array1, strings, str));
     }
 
 
